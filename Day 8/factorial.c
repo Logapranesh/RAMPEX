59cd5d4ee398b7ae10342a1d8ef310e992cc2612
Loga Pranesh S <122467628+Logapranesh@users.noreply.github.com>
2025-05-20 15:50:48 +0530

    Add files via upload

---
+++ Day 8/factorial.c
@@ -0,0 +1,18 @@
+//factorial using function
+
+#include <stdio.h>
+
+int factorial(int n)
+{
+    int fact=1;
+    for(int i=1;i<=5;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+int main(){
+    int num=5;
+    int result = factorial(num); 
+    printf("Factorial of %d is %d\n", num, result);
+    return 0;
+}
